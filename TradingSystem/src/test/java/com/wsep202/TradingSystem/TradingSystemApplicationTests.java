9bec06957e448e7389aa666562289c3aae062e3d
RonAsis <37269047+RonAsis@users.noreply.github.com>
2020-03-27 10:52:31 +0300

    initial project

---
+++ TradingSystem/src/test/java/com/wsep202/TradingSystem/TradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsep202.TradingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
