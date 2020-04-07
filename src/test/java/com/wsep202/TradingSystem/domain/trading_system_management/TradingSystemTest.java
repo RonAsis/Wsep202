a0ae656f6cfca83c873edb4307feb70d4b5049de
Ron Asis <Ron.Asis@ecitele.com>
2020-04-07 10:07:46 +0300

    add tests to trading system

---
+++ src/test/java/com/wsep202/TradingSystem/domain/trading_system_management/TradingSystemTest.java
@@ -0,0 +1,187 @@
+package com.wsep202.TradingSystem.domain.trading_system_management;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TradingSystemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Nested
+    public class TradingSystemTestUnit {
+
+        @Test
+        void buyShoppingCart() {
+        }
+
+        @Test
+        void registerNewUser() {
+        }
+
+        @Test
+        void login() {
+        }
+
+        @Test
+        void closeStore() {
+        }
+
+        @Test
+        void logout() {
+        }
+
+        @Test
+        void getAdministratorUser() {
+        }
+
+        @Test
+        void getStore() {
+        }
+
+        @Test
+        void testGetStore() {
+        }
+
+        @Test
+        void getUser() {
+        }
+
+        @Test
+        void getUserByAdmin() {
+        }
+
+        @Test
+        void searchProductByName() {
+        }
+
+        @Test
+        void searchProductByCategory() {
+        }
+
+        @Test
+        void searchProductByKeyWords() {
+        }
+
+        @Test
+        void filterByRangePrice() {
+        }
+
+        @Test
+        void filterByProductRank() {
+        }
+
+        @Test
+        void filterByStoreRank() {
+        }
+
+        @Test
+        void filterByStoreCategory() {
+        }
+
+        @Test
+        void purchaseShoppingCart() {
+        }
+
+        @Test
+        void testPurchaseShoppingCart() {
+        }
+
+        @Test
+        void openStore() {
+        }
+    }
+
+    @Nested
+    public class TradingSystemTestIntegration {
+
+        @Test
+        void buyShoppingCart() {
+        }
+
+        @Test
+        void registerNewUser() {
+        }
+
+        @Test
+        void login() {
+        }
+
+        @Test
+        void closeStore() {
+        }
+
+        @Test
+        void logout() {
+        }
+
+        @Test
+        void getAdministratorUser() {
+        }
+
+        @Test
+        void getStore() {
+        }
+
+        @Test
+        void testGetStore() {
+        }
+
+        @Test
+        void getUser() {
+        }
+
+        @Test
+        void getUserByAdmin() {
+        }
+
+        @Test
+        void searchProductByName() {
+        }
+
+        @Test
+        void searchProductByCategory() {
+        }
+
+        @Test
+        void searchProductByKeyWords() {
+        }
+
+        @Test
+        void filterByRangePrice() {
+        }
+
+        @Test
+        void filterByProductRank() {
+        }
+
+        @Test
+        void filterByStoreRank() {
+        }
+
+        @Test
+        void filterByStoreCategory() {
+        }
+
+        @Test
+        void purchaseShoppingCart() {
+        }
+
+        @Test
+        void testPurchaseShoppingCart() {
+        }
+
+        @Test
+        void openStore() {
+        }
+    }
+}
