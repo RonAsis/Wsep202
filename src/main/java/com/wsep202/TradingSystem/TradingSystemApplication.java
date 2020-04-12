43fb09506c1ae9812311012c768a96fc6475dbb3
MoranNeptune <neptune@post.bgu.ac.il>
2020-04-12 10:08:40 +0300

    another version of logger

--- src/main/java/com/wsep202/TradingSystem/TradingSystemApplication.java
+++ src/main/java/com/wsep202/TradingSystem/TradingSystemApplication.java
@@ -1,18 +1,16 @@
 package com.wsep202.TradingSystem;
+//import lombok.extern.slf4j.Slf4j;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Slf4j
 @SpringBootApplication
 public class TradingSystemApplication {
 
-	//private static final Logger logger = LoggerFactory.getLogger(TradingSystemApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TradingSystemApplication.class, args);
-		/*logger.info("this is a info message");
-		logger.info("this is a info message");
-		logger.error("this is a error message");*/
+		/*log.info("sjknsfjkksfkjsfnsfk");
+		log.error("sfkvnfjknvkfnvkjfvnnvkjfnvjk");*/
 	}
 }
