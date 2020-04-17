febc8f199f1a530933c07bca11e0e37e7e6a33ef
KseniaDr <kseniad@post.bgu.ac.il>
2020-04-17 22:49:53 +0300

    billingAddress & paymentDetails dto

---
+++ src/main/java/com/wsep202/TradingSystem/service/user_service/dto/BillingAddressDto.java
@@ -0,0 +1,25 @@
+package com.wsep202.TradingSystem.service.user_service.dto;
+
+public class BillingAddressDto {
+
+    /**
+     * the name of customer
+     */
+    private String customerFullName;
+    /**
+     * the street name
+     */
+    private String address;
+    /**
+     * the users city for the delivery
+     */
+    private String city;
+    /**
+     * the users country for the delivery
+     */
+    private String country;
+    /**
+     * the users zip code for the delivery
+     */
+    private String zipCode;
+}
---
+++ src/main/java/com/wsep202/TradingSystem/service/user_service/dto/PaymentDetailsDto.java
@@ -0,0 +1,45 @@
+package com.wsep202.TradingSystem.service.user_service.dto;
+
+import com.wsep202.TradingSystem.domain.trading_system_management.CardAction;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PaymentDetailsDto {
+
+    /**
+     * the action the credit card was used for
+     */
+    private CardAction creditAction;
+    /**
+     * the number of the card
+     */
+    private String creditCardNumber;
+    /**
+     * the month of the credit card
+     */
+    private String month;
+    /**
+     * the year of the year
+     */
+    private String year;
+
+    /**
+     * the name of the credit card holder
+     */
+    private String Cardholder;
+
+    /**
+     * ccv of the, the digits on the back of the card
+     */
+    private int ccv;
+    /**
+     * the id of the credit card holder
+     */
+    private String holderIDNumber;
+}
