e6a86e52b76312b4f5fde56d152089010c0ade71
MoranNeptune <neptune@post.bgu.ac.il>
2020-04-17 15:56:33 +0300

    fix annotations

--- src/main/java/com/wsep202/TradingSystem/service/user_service/dto/PurchasePolicyDto.java
+++ src/main/java/com/wsep202/TradingSystem/service/user_service/dto/PurchasePolicyDto.java
@@ -1,12 +1,14 @@
 package com.wsep202.TradingSystem.service.user_service.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PurchasePolicyDto {
     /**
      * allow everyone to purchase from store
