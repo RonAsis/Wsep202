aa71c50b8d68b181732b4274b372832cae94217d
Ron Asis <Ron.Asis@ecitele.com>
2020-04-24 11:15:51 +0300

    use mock

--- src/test/java/com/wsep202/TradingSystem/TradingSystemApplicationTest.java
+++ src/test/java/com/wsep202/TradingSystem/TradingSystemApplicationTest.java
@@ -1,12 +1,15 @@
 package com.wsep202.TradingSystem;
 
+import com.wsep202.TradingSystem.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * check if all the application is running
  */
 @SpringBootTest(args = {"admin", "admin"})
+@ContextConfiguration(classes = {TestConfig.class})
 class TradingSystemApplicationTest {
 
     @Test
