ab173bfc0c42198bf9cbc6cf2301952575201034
barPerlman <barper@post.bgu.ac.il>
2020-06-22 07:59:00 +0300

    added facade tests class

---
+++ trading-system-server/src/test/java/com/wsep202/TradingSystem/domain/trading_system_management/TradingSystemFacadeTest.java
@@ -0,0 +1,250 @@
+package com.wsep202.TradingSystem.domain.trading_system_management;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TradingSystemFacadeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void viewPurchaseHistory() {
+    }
+
+    @Test
+    void testViewPurchaseHistory() {
+    }
+
+    @Test
+    void testViewPurchaseHistory1() {
+    }
+
+    @Test
+    void viewPurchaseHistoryOfManager() {
+    }
+
+    @Test
+    void viewPurchaseHistoryOfOwner() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getStoreDiscounts() {
+    }
+
+    @Test
+    void removeDiscount() {
+    }
+
+    @Test
+    void addEditDiscount() {
+    }
+
+    @Test
+    void addEditPurchase() {
+    }
+
+    @Test
+    void getCompositeOperators() {
+    }
+
+    @Test
+    void deleteProductFromStore() {
+    }
+
+    @Test
+    void editProduct() {
+    }
+
+    @Test
+    void addOwner() {
+    }
+
+    @Test
+    void addManager() {
+    }
+
+    @Test
+    void addPermission() {
+    }
+
+    @Test
+    void removePermission() {
+    }
+
+    @Test
+    void getPermissionOfManager() {
+    }
+
+    @Test
+    void removeManager() {
+    }
+
+    @Test
+    void removeOwner() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void openStore() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void saveProductInShoppingBag() {
+    }
+
+    @Test
+    void watchShoppingCart() {
+    }
+
+    @Test
+    void removeProductInShoppingBag() {
+    }
+
+    @Test
+    void purchaseShoppingCart() {
+    }
+
+    @Test
+    void testPurchaseShoppingCart() {
+    }
+
+    @Test
+    void getOwnerStores() {
+    }
+
+    @Test
+    void getMangeStores() {
+    }
+
+    @Test
+    void getStores() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void connectNotificationSystem() {
+    }
+
+    @Test
+    void sendNotification() {
+    }
+
+    @Test
+    void getCategories() {
+    }
+
+    @Test
+    void getOperationsCanDo() {
+    }
+
+    @Test
+    void getAllOperationOfManger() {
+    }
+
+    @Test
+    void getTotalPriceOfShoppingCart() {
+    }
+
+    @Test
+    void addProductToShoppingCart() {
+    }
+
+    @Test
+    void getShoppingCart() {
+    }
+
+    @Test
+    void testGetTotalPriceOfShoppingCart() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void getDiscountsSimple() {
+    }
+
+    @Test
+    void getAlltDiscounts() {
+    }
+
+    @Test
+    void getAllStorePurchases() {
+    }
+
+    @Test
+    void getAllUsernameNotOwnerNotManger() {
+    }
+
+    @Test
+    void getMySubOwners() {
+    }
+
+    @Test
+    void getMySubMangers() {
+    }
+
+    @Test
+    void getPermissionCantDo() {
+    }
+
+    @Test
+    void isOwner() {
+    }
+
+    @Test
+    void changeProductAmountInShoppingBag() {
+    }
+
+    @Test
+    void getMyOwnerToApprove() {
+    }
+
+    @Test
+    void approveOwner() {
+    }
+
+    @Test
+    void getDailyVisitors() {
+    }
+
+    @Test
+    void updateDailyVisitor() {
+    }
+
+    @Test
+    void sendDailyVisitor() {
+    }
+
+    @Test
+    void stopDailyVisitors() {
+    }
+}
